e6d3303bb05bc57ac59789299096e16488762db7
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-01 07:36:58 +0300

    Adding regression line on a plot.

--- gitter.R
+++ gitter.R
@@ -1,4 +1,12 @@
-a = 2+3
-b <- 3-6
-c <- a/b
-d <- 23+69
+data("iris")
+# Drawing regression line on a plot
+
+teg <-  lm(Sepal.Length ~ Petal.Width, data = iris)
+plot(iris$Sepal.Length ~ iris$Petal.Width)
+abline(teg, 
+       col = "blue", 
+       lwd = 3, 
+       lty = "dashed",
+       v = 1.5,
+       h = 7.3)
+
