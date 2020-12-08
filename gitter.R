afa61dc64a58238da268b3b4025a816d52af70fe
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-08 18:34:43 +0300

    Changed the point character of the scatter to 16, i.e. pch = 16.

--- gitter.R
+++ gitter.R
@@ -2,7 +2,9 @@ data("iris")
 # Drawing regression line on a plot
 
 Reg <-  lm(Sepal.Length ~ Petal.Width, data = iris)
-plot(iris$Sepal.Length ~ iris$Petal.Width, col = "red")
+plot(iris$Sepal.Length ~ iris$Petal.Width, 
+     col = "purple",
+     pch = 16)
 abline(Reg, 
        col = "blue", 
        lwd = 3, 
