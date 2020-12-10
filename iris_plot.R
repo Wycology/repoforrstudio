1eb5623a858cf7e2d330419a7cf39cd2d8799aa4
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-10 16:44:00 +0300

    Legends moved from the plots.

--- iris_plot.R
+++ iris_plot.R
@@ -61,10 +61,20 @@ uniform_smooth <- ggplot(data = iris,
 
 combined_plots <- separate_smooth + uniform_smooth
 
+# Now I want to give the plots title and tags or labels to identify them
 
+combined_plots_labeled <- combined_plots +
+  plot_annotation(
+    title = "Dimensions of three iris species",
+    tag_levels = "A"
+  ) # This makes the first plot to be a and the next to be b
 
+# Now I want to remove the legends since the species labels are visible
 
+combined_plots_labeled_no_legend <- combined_plots_labeled &
+  theme(legend.position = "none")
 
+combined_plots_labeled_no_legend
 
 
 
