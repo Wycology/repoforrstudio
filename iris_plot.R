dd2b86bddfe0f115b12e69cef56c455faf717187
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-10 16:36:56 +0300

    Adding the second plot.

--- iris_plot.R
+++ iris_plot.R
@@ -16,7 +16,7 @@ str(iris)
 
 plot(iris$Sepal.Length, iris$Petal.Width)
 
-separate_geoms <- ggplot(data = iris,
+separate_smooth <- ggplot(data = iris,
        mapping = aes(x = Sepal.Length,
                      y = Petal.Width,
                      color = Species)) +
@@ -35,12 +35,31 @@ separate_geoms <- ggplot(data = iris,
                         filter = Species == "virginica",
                         label = "Iiris virginica",
                         description = "Tallest species")) +
+  theme_classic() +
   geom_smooth()
 
-combined_geoms <- 
+uniform_smooth <- ggplot(data = iris,
+                         mapping = aes(x = Sepal.Length,
+                                       y = Petal.Width)) +
+  geom_point() +
+  labs(x = "Petal length (cm)",
+       y = "Petal width (cm)") +
+  geom_mark_ellipse(aes(fill = Species,
+                        filter = Species == "setosa",
+                        label = "Iris setosa",
+                        description = "Shortest species")) +
+  geom_mark_ellipse(aes(fill = Species,
+                        filter = Species == "versicolor",
+                        label = "Iris versicolor",
+                        description = "Medium species")) +
+  geom_mark_ellipse(aes(fill = Species,
+                        filter = Species == "virginica",
+                        label = "Iiris virginica",
+                        description = "Tallest species")) +
+  theme_classic() +
+  geom_smooth()
 
-min(iris$Petal.Length[iris$Species=="setosa"])
-min(iris$Sepal.Width[iris$Species=="setosa"])
+combined_plots <- separate_smooth + uniform_smooth
 
 
 
