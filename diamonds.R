0eb37ea30cda678a1c32fe30e62a5a1dcd3b26b3
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-12 17:02:34 +0300

    Adding jitter to spread points.

--- diamonds.R
+++ diamonds.R
@@ -1,8 +1,11 @@
+# Combining both boxplot and violin plots in one.
+
 library(tidyverse)
 diamonds_df <- diamonds
 
-plotty <- diamonds_df %>% 
+diamonds_df %>% 
   ggplot(aes(x = cut, y = price, color = cut)) +
+  geom_point(position = "jitter", alpha = 0.1)+
   geom_boxplot(alpha = 0, width = 0.15) +
   geom_violin(alpha = 0) +
   theme(legend.position = "none")
