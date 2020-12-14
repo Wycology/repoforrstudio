55aa0d5c34e206b19fdbb0f8176dc5df1bf1488c
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-14 14:49:21 +0300

    Description of the grouping by color() argument in the aes() function.

--- iris_plot.R
+++ iris_plot.R
@@ -18,14 +18,14 @@ plot(iris$Sepal.Length, iris$Petal.Width) # Basic plot within the R base.
 
 # Creating the first plot of the iris data.
 
-separate_smooth <- ggplot(data = iris,
-       mapping = aes(x = Sepal.Length,
+separate_smooth <- ggplot(data = iris,    # Specifying the data to be plotted.
+       mapping = aes(x = Sepal.Length,    # Stating the axes data.
                      y = Petal.Width,
-                     color = Species)) +
-  geom_point() +
-  labs(x = "Petal length (cm)",
+                     color = Species)) +  # Ensuring points are colored by spp.
+  geom_point() +                          # Deciding the plotting geometry.
+  labs(x = "Petal length (cm)",           # Specifying the axes labels.
        y = "Sepal width (cm)") +
-  geom_mark_ellipse(aes(fill = Species,
+  geom_mark_ellipse(aes(fill = Species,   # Adding ellipses for the spps.
                         filter = Species == "setosa",
                         label = "Iris setosa",
                         description = "Shortest species")) +
@@ -37,12 +37,15 @@ separate_smooth <- ggplot(data = iris,
                         filter = Species == "virginica",
                         label = "Iiris virginica",
                         description = "Tallest species")) +
-  theme_classic() +
-  geom_smooth()
+  theme_classic() +                      # Having publication ready background.
+  geom_smooth()                          # Adding smooth line around 95% level.
+
+# In the above plot, having color within the aes() breakes the data into 
+# species-wise groups. Meaning each species group will be smoothed separately.
 
 uniform_smooth <- ggplot(data = iris,
                          mapping = aes(x = Sepal.Length,
-                                       y = Petal.Width)) +
+                                       y = Petal.Width)) + # Note no color here.
   geom_point() +
   labs(x = "Petal length (cm)",
        y = "Petal width (cm)") +
@@ -60,6 +63,9 @@ uniform_smooth <- ggplot(data = iris,
                         description = "Tallest species")) +
   theme_classic() +
   geom_smooth()
+
+# Avoiding the color parameter in the aes() ensures all species are considered
+# together in the subsequent layers arguments. No grouping by species.
 
 combined_plots <- separate_smooth + uniform_smooth
 
