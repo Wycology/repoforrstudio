604d1c237e4077f1c0d54a8b6eda0fb52d4d7b10
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-16 11:15:29 +0300

    Regression line on orange dataset.

--- Orange.R
+++ Orange.R
@@ -2,8 +2,8 @@ data("Orange")     # Loading the inbuilt Orange data
 head(Orange)       # First 6 rows of the dataframe
 length(Orange$age) # Checking the length of the dataframe
 
-Orangelm <- lm(circumference ~ age, data = Orange)
-summary(Orangelm)
+Orangelm <- lm(circumference ~ age, data = Orange) # Regression model for orange
+summary(Orangelm) # Details of the regression model.
 
 plot(Orange$circumference ~ Orange$age, 
      col = "purple",
@@ -12,5 +12,7 @@ plot(Orange$circumference ~ Orange$age,
      xlab = "Age (days)",
      ylab = "Circumference (mm)",
      main = "Relationship between age and cricumference of oranges")
-abline(Orangelm, lwd = 3, lty = "dashed", col = "cyan")
+abline(Orangelm, lwd = 3, lty = "dashed", col = "cyan") # Adding the lm as abline
 text(300, 200, "R^2 = 0.8345\nP<0.0001\ny = 17.4 + 0.107x")
+# Annotating text on the graph.
+# \n indicates line break to shift the next component to next line.
