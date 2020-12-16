8bf49145b3b67d3a804a9fe0c5bb3a9005b409b8
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-16 11:12:35 +0300

    Regression model on oranges scatter plot.

--- Orange.R
+++ Orange.R
@@ -1,3 +1,16 @@
 data("Orange")     # Loading the inbuilt Orange data
 head(Orange)       # First 6 rows of the dataframe
 length(Orange$age) # Checking the length of the dataframe
+
+Orangelm <- lm(circumference ~ age, data = Orange)
+summary(Orangelm)
+
+plot(Orange$circumference ~ Orange$age, 
+     col = "purple",
+     pch = 19,
+     cex = 2,
+     xlab = "Age (days)",
+     ylab = "Circumference (mm)",
+     main = "Relationship between age and cricumference of oranges")
+abline(Orangelm, lwd = 3, lty = "dashed", col = "cyan")
+text(300, 200, "R^2 = 0.8345\nP<0.0001\ny = 17.4 + 0.107x")
