68d012ed578f623a4683344ee381be70d2f8d700
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-26 10:51:02 +0300

    Super crazy model in R using modelr package.

--- iris_plot.R
+++ iris_plot.R
@@ -134,7 +134,7 @@ ggplot(data = sim1, mapping = aes(x = x, y =y)) +
   geom_abline(mapping = aes(intercept = a1, slope = a2),
               data = models, alpha = 1/4) +
   geom_point(col = 'purple', pch = 18, size = 5) +
-  labs(x = 'The x-axis', y = 'The y-axis', title = 'Crazy model') +
+  labs(x = 'The x-axis', y = 'The y-axis', title = 'Crazy model in deed') +
   theme_classic()
 
 
