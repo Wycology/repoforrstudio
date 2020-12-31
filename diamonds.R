7d548be017276cb4ded275a36e4cfd97c1bddc0e
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2020-12-31 19:30:22 +0300

    Changing the alpha for jitter value.

--- diamonds.R
+++ diamonds.R
@@ -5,7 +5,7 @@ diamonds_df <- diamonds
 
 diamonds_df %>% 
   ggplot(aes(x = cut, y = price, color = cut)) +
-  geom_point(position = "jitter", alpha = 0.1)+
+  geom_point(position = "jitter", alpha = 0.2)+
   geom_boxplot(alpha = 0, width = 0.15) +
   geom_violin(alpha = 0.5) +
   theme_classic() +
