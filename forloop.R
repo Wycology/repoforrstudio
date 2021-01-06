e678267801cbd1b830b393c5030bfb0159fd3d7d
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-06 19:41:48 +0300

    Idolizing the for loop.

--- forloop.R
+++ forloop.R
@@ -22,7 +22,7 @@ volumes <- c(1.6, 3, 8)
 
 for (volume in volumes){  # I have just replaced item with volume, list_of_items
   print(2.65 * volume^0.9)# with volumes and do_something with print with some
-}                         # math on the items
+}                         # math on the items.
 
 # What is going on in the above code is that every element in the volumes vector
 # is raised to power 0.9 then multiplied by 2.65 and printed out.
@@ -41,9 +41,15 @@ print(2.65 * volume^0.9) # Doing the math on that third element picked from volu
 
 # It is clear that the second approach of doing the same thing is quite tiring.
 
+# If subsetting with square brackets [] is still a nightmare, then we can achieve
+# the end result with plain math like this:
 
+2.65 * 1.6^0.9 # Really picking the first element in volumes vector manually
+2.65 * 3^0.9   # Picking the second element in the volumes vector by eye
+2.65 * 8^0.9   # Literally lifting the third element and doing the math.
 
-
+# So in the end, we are sweating and at the back of our minds we wish we could go 
+# back to the for loop.
 
 
 
