f2a64b3da95496fa2184527ca89377cc1213d96d
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-06 19:30:44 +0300

    Comparing for loop and picking of individual elements from a vector to achieve the same objective.

--- forloop.R
+++ forloop.R
@@ -20,11 +20,26 @@ for (item in list_of_items){
 
 volumes <- c(1.6, 3, 8)
 
+for (volume in volumes){
+  print(2.65 * volume^0.9)
+}
 
+# What is going on in the above code is that every element in the volumes vector
+# is raised to power 0.9 then multiplied by 2.65 and printed out.
 
+# So we can get the same results by calling every element in the vector individually
+# and raising to power 0.9 and multiplying by 2.65.
 
+volume <- volumes[1]     # Picking the first element in the volumes vector
+print(2.65 * volume^0.9) # Doing the math on that first element
 
+volume <- volumes[2]     # Picking the second element in the volumes vector
+print(2.65 * volume^0.9) # Doing the math on that second element
 
+volume <- volumes[3]     # Picking the third element in the volumes vector
+print(2.65 * volume^0.9) # Doing the math on that third element picked from volumes
+
+# It is clear that the second approach of doing the same thing is quite tiring.
 
 
 
