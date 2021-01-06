6f0982648cd99cd2e48c17430aec24b929985296
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-06 19:15:26 +0300

    Creating a simple vector having three numbers to demonstrate functionality of for loop in R.

--- forloop.R
+++ forloop.R
@@ -6,3 +6,31 @@
 # Last edited 6th Jan 2021    #
 ###############################
 
+# Basic structure of a for loop is of the kind:
+
+for (item in list_of_items){
+  do_something(item)
+  print()
+}
+
+# Like for every student in a class, vaccinate. So every student must get the 
+# vaccine. 
+
+# For better appreciation, let me create a vector called volumes of three numbers.
+
+volumes <- c(1.6, 3, 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
