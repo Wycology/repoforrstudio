45a0735d666a6cbf51b2bdce1af83260364377b0
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-07 21:23:47 +0300

    Adding more description of the functions especially with respect to the order of input values within a function.

--- functionsinr.R
+++ functionsinr.R
@@ -7,8 +7,9 @@
 # Last edited: 7th Jan 2020             #
 #########################################
 
-# R has thousands of inbuilt functions, how to write them is important just to
-# be sure of what a function does.
+# R has thousands of inbuilt functions as well as those inside various third 
+# party libraries, how to write them is important just to be sure of what a 
+# function does.
 
 # Let us start with a basic structure of a function.
 
@@ -16,3 +17,43 @@ function_name <- function(inputs){
   output_value <- do_something(inputs)
   return(output_value)
 }
+
+# Using curly braces {} tells R that whatever is inside should be run together, e.g
+
+{
+  a = 3
+  b = 2
+  a + b
+}
+
+# Let us create a function which calculates the volume of a shrub
+
+calc_shrub_vol <- function(length, width, height){ # inputs are the dimensions
+  volume <- length*width*height # output is given the math to do on inputs
+  return(volume) # output is returned
+}
+
+# Now we have created a function, so we need to feed it some data to see output
+# Let us assume a shrub has a length of 1.8m, width of 1.2m, and height of 2.0m
+
+# So we feed these dimension values into the function to see the output
+
+calc_shrub_vol(1.8, 1.2, 2.0) # the order follows that of defined functions
+
+# However when we want to change the order, like enter height, then length, then
+# width then we must provide them in the function explicitly like:
+
+calc_shrub_vol(height = 2.0, length = 1.8, width = 1.2) # Same results. Care must
+# be taken when playing around with the input values to take care of the order.
+
+
+
+
+
+
+
+
+
+
+
+
