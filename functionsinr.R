ce59568891883302ce4beb69dbbc48154115a7ce
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-07 20:30:03 +0300

    Writing functions in R. Super important skill to develop.

---
+++ functionsinr.R
@@ -0,0 +1,12 @@
+#########################################
+# Name: Wyclife Agumba Oluoch           #   
+# E-mail: wyclifeoluoch@gmail.com       #
+# GitHub: https://github.com/Wycology   #
+# Task: Creating functions in R         #
+# Date: 7th Jan 2020                    #
+# Last edited: 7th Jan 2020             #
+#########################################
+
+# R has thousands of inbuilt functions, how to write them is important just to
+# be sure of what a function does.
+
