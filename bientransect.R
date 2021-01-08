267d9c66df6127dac05b3b6f8df6325f75375d35
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-08 21:44:12 +0300

    Downloading the BIEN data.

--- bientransect.R
+++ bientransect.R
@@ -1,7 +1,7 @@
 ###############################
 # Wyclife Agumba Oluoch       #
 # wyclifeoluoch@gmail.com     #
-# Task working with for loop  #
+# Task working with BIEN  data#
 # Created 8th Jan 2021        #
 # Last edited 8th Jan 2021    #
 ###############################
@@ -9,5 +9,16 @@
 
 # Working with transect data in bien library
 # install.packages('BIEN'), just realized I did not have the package
-library(BIEN)
+
+# Loading the required libraries
+
+library(BIEN); library(raster); library(dplyr); library(ggplot2)
+
+# Next, I am going to download the gentry transect dataset
+
+gentry <- BIEN_plot_dataset('Gentry Transect Dataset', 
+                            political.boundaries = TRUE)
+sites <- na.omit(unique(gentry[c('plot_name', 
+                                 'latitude', 
+                                 'longitude')]))
 
