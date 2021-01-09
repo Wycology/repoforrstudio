9eee8f3751650b87231f48a9876a8b57181f2672
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-09 22:36:11 +0300

    Wrangling data with some verbs in dplyr.

---
+++ dplyr.R
@@ -0,0 +1,10 @@
+#########################################
+# Name: Wyclife Agumba Oluoch           #   
+# E-mail: wyclifeoluoch@gmail.com       #
+# GitHub: https://github.com/Wycology   #
+# Task: Some data wrangling with dplyr  #
+# Date: 9th Jan 2020                    #
+# Last edited: 9th Jan 2020             #
+#########################################
+
+library(dplyr)
