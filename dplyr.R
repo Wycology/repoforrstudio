1c309aa471f54b598cf3fcbb7927e4d952c96f57
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-17 15:41:59 +0300

    Revising the dplyr on titanic data.

--- dplyr.R
+++ dplyr.R
@@ -4,10 +4,11 @@
 # GitHub: https://github.com/Wycology   #
 # Task: Some data wrangling with dplyr  #
 # Date: 9th Jan 2020                    #
-# Last edited: 9th Jan 2020             #
+# Last edited: 17th Jan 2020             #
 #########################################
 
-library(dplyr) # The great package for data wrangling
+library(dplyr) # For data wrangling in the code
+
 
 # I will use the Titanic data
 
@@ -18,7 +19,7 @@ data <- tibble(data) # Here is the tibble form of the data.
 dim(data) # Checking the dimension of the data set.
 
 # Checking the total number of passangers in the Titanic
-# To do this, we need the sum of the frequency variable
+# To do this, I need the sum of the frequency variable
 
 number_passengers <- summarise(data, number_passengers = sum(Freq))
 number_passengers # Returns the total passangers to be 2201.
