8382ee79e50f3740cf6517621a1286a4eee15138
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-20 15:59:10 +0300

    Simplifying for loop work.

--- forloop.R
+++ forloop.R
@@ -27,8 +27,8 @@ for (volume in volumes){  # I have replaced item with volume, list_of_items
 # What is going on in the above code is that every element in the volumes vector
 # is raised to power 0.9 then multiplied by 2.65 and printed out.
 
-# So we can get the same results by calling every element in the vector individually
-# and raising to power 0.9 and multiplying by 2.65.
+# So we can get the same results by calling every element in the vector 
+# individually and raising to power 0.9 and multiplying by 2.65.
 
 volume <- volumes[1]     # Picking the first element in the volumes vector
 print(2.65 * volume^0.9) # Doing the math on that first element
@@ -41,15 +41,15 @@ print(2.65 * volume^0.9) # Doing the math on that third element
 
 # It is clear that the second approach of doing the same thing is quite tiring.
 
-# If subsetting with square brackets [] is still a nightmare, then we can achieve
-# the end result with plain math like this:
+# If subsetting with square brackets [] is still a nightmare, then we can 
+# achieve the end result with plain math like this:
 
 2.65 * 1.6^0.9 # Really picking the first element in volumes vector manually
 2.65 * 2^0.9   # Picking the second element in the volumes vector by eye
 2.65 * 29^0.9  # Literally lifting the third element and doing the math.
 
-# So in the end, we are sweating and at the back of our minds we wish we could go 
-# back to the for loop.
+# So in the end, we are sweating and at the back of our minds we wish we could 
+# go back to the for loop.
 
 # There we go...
 
@@ -57,7 +57,7 @@ for (volume in volumes){
   print(2.65 * volume^0.9)
 }
 
-# Super simple... :)
+# Super simple... :) Right!
 
 # Now we can do more with the for loop including creating outputs which we save 
 # to objects and other objects, and other objects.....
