8a71fda3c935cfffde5300d3e23c06dd27a47a95
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-01-21 18:31:17 +0300

    Writing functions in R, such a cool effort. The one area where R flies above non-programming platforms.

--- functionsinr.R
+++ functionsinr.R
@@ -4,10 +4,10 @@
 # GitHub: https://github.com/Wycology   #
 # Task: Creating functions in R         #
 # Date: 7th Jan 2020                    #
-# Last edited: 7th Jan 2020             #
+# Last edited: 21st Jan 2020            #
 #########################################
 
-# R has thousands of inbuilt functions as well as those inside various third 
+# R has thousands of in-built functions as well as those inside various third 
 # party libraries, how to write them is important just to be sure of what a 
 # function does.
 
