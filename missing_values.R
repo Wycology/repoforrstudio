83ee871d725f9c0bcad9eb3cff8f6a9e43ffd895
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-09 16:39:16 +0300

    Documenting working with R in R dataset.

---
+++ missing_values.R
@@ -0,0 +1,9 @@
+#################################
+# Wyclife Agumba Oluoch         #
+# wyclifeoluoch@gmail.com       # 
+# Task working with NAs in data #
+# Created 8th Jan 2021          #
+# Last edited 9th Feb 2021      #
+#################################
+
+# Here I am going to demonstrate how to work with missing data (NAs) in R
