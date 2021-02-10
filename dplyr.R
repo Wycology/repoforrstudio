bd4662e8f3196790c8fe5cc90f7babede08f6c86
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-10 18:03:42 +0300

    dplyr at another level.

--- dplyr.R
+++ dplyr.R
@@ -4,7 +4,7 @@
 # GitHub: https://github.com/Wycology   #
 # Task: Some data wrangling with dplyr  #
 # Date: 9th Jan 2020                    #
-# Last edited: 27th Jan 2020             #
+# Last edited: 27th Jan 2020            #
 #########################################
 
 library(dplyr) # For data wrangling in the whole project.
