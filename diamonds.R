cdfc9e645016a927f6a7d7b5599a89383c173a4b
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-10 18:09:03 +0300

    Better view of the boxplot.

--- diamonds.R
+++ diamonds.R
@@ -16,7 +16,7 @@ diamonds_df <- diamonds # Cloning the diamonds data to a new object
 diamonds_df %>% # Picking the data
   ggplot(aes(x = cut, y = price, color = cut)) + # Adding the mapping variables
   geom_point(position = "jitter", alpha = 0.2) + # Adding jitter and alpha
-  geom_boxplot(alpha = 0.1, width = 0.5) +
+  geom_boxplot(alpha = 0.1, width = 0.5) + # Modifying dimensions of the boxplot 
   geom_violin(alpha = 0.5) +
   theme_classic() +
   theme(legend.position = "none") +
