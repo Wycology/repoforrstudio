edc7d6d42748a586ac5a17c7ab8b50361a979c32
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-12 11:14:56 +0300

    Be careful when imputing missing values in a dataset so that there is no compromise in the subsequent analyses.

--- missing_values.R
+++ missing_values.R
@@ -74,14 +74,5 @@ t.test(na_data$Pre.Test, na_data$Post.Test, paired = TRUE)
 t.test(na_data_gam$Pre.Test, na_data_gam$Post.Test, paired = TRUE)
 
 # It is now clear that t, df, p-value, 95% confidence interval are all different.
-
-
-
-
-
-
-
-
-
-
-
+# This is calling for maximum care when imputing missing values in a dataset
+# depending on the subsequent analyses to be conducted.
