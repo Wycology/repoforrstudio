5dad6f6517305c668e100f63b1f3d9f8b188781e
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-12 11:02:37 +0300

    Missing values in R

--- missing_values.R
+++ missing_values.R
@@ -3,7 +3,7 @@
 # wyclifeoluoch@gmail.com       # 
 # Task working with NAs in data #
 # Created 8th Jan 2021          #
-# Last edited 9th Feb 2021      #
+# Last edited 12th Feb 2021     #
 #################################
 
 # Here I am going to demonstrate how to work with missing data (NAs) in R
