1bd741235f95f9931bf7af60f923162eaa36eb4e
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-15 19:38:45 +0300

    Just a small portion of the data to enhance speed of analyses and plotting.

--- diamonds.R
+++ diamonds.R
@@ -17,7 +17,7 @@ head(diamonds) # Checking the variables in the data-set for subsequent analyses
 nrow(diamonds) # Checking for the number of rows in the data. 53940 observations
 
 diamonds_df %>% # Picking the diamonds data object
-  slice_sample(n = 0.1*(nrow(diamonds))) %>% 
+  slice_sample(n = 0.1*(nrow(diamonds))) %>% # Picking a portion of the data-set
   ggplot(aes(x = cut, y = price, color = cut)) + # Adding the mapping variables
   geom_point(position = "jitter", alpha = 0.2) + # Adding jitter and alpha
   geom_boxplot(alpha = 0.1, width = 0.5) + # Modifying dimensions of the boxplot 
