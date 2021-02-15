77bb96527c4a200b265dc1acb592fc8dcf2348ba
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-15 19:37:07 +0300

    Slicing a small portion of the data-set.

--- diamonds.R
+++ diamonds.R
@@ -13,9 +13,11 @@ library(RColorBrewer) # This is for generating color pallets for the plots.
 
 diamonds_df <- diamonds # Saving the diamonds data to a new object
 
-head(diamonds) # Checking the variables in the dataset for subsequent analyses
+head(diamonds) # Checking the variables in the data-set for subsequent analyses
+nrow(diamonds) # Checking for the number of rows in the data. 53940 observations
 
 diamonds_df %>% # Picking the diamonds data object
+  slice_sample(n = 0.1*(nrow(diamonds))) %>% 
   ggplot(aes(x = cut, y = price, color = cut)) + # Adding the mapping variables
   geom_point(position = "jitter", alpha = 0.2) + # Adding jitter and alpha
   geom_boxplot(alpha = 0.1, width = 0.5) + # Modifying dimensions of the boxplot 
