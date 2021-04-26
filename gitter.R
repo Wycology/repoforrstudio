afc1b4f7feb084201991b859dbbcb08345bebdd0
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-26 17:33:56 +0300

    gittr

--- gitter.R
+++ gitter.R
@@ -4,7 +4,7 @@
 # https://github.com/Wycology #
 # Task: Playing with some data#
 # Created: 30th Nov 2020      #
-# Edited: 24th April 2021     #
+# Edited: 26th April 2021     #
 ###############################
 
 # Loading the necessary libraries for the project
