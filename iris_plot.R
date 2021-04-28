bf9f0ff859bc7b9be552dc4bfe8d940b7a7dd407
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-28 17:14:55 +0300

    #

--- iris_plot.R
+++ iris_plot.R
@@ -1,8 +1,8 @@
 ################################
-# Wyclife Agumba Oluoch       #
-# wyclifeoluoch@gmail.com     #
-# https://github.com/Wycology #
-# Task: Playing with iris data#
+# Wyclife Agumba Oluoch        #
+# wyclifeoluoch@gmail.com      #
+# https://github.com/Wycology  #
+# Task: Playing with iris data #
 # Created: 30th Dec 2020      #
 # Edited: 27th April 2021     #
 ###############################
