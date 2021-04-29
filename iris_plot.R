331d342b3952709d4ca6acf5b2b84104847b73aa
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-29 19:07:12 +0300

    work-flow

--- iris_plot.R
+++ iris_plot.R
@@ -11,7 +11,7 @@
 
 # Loading necessary packages.
 
-library(modelr)    # for modeling workflow
+library(modelr)    # for modeling work-flow
 library(patchwork) # Combining plots using the package
 library(ggforce)   # combining the plots 
 library(tidyverse) # Huge library for wrangling and visualizing data the data-set
