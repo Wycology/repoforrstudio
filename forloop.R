e46074137a796bc4a3fd49ef36e8d1dfdb167963
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-05-15 15:54:28 +0300

    15th is the date

--- forloop.R
+++ forloop.R
@@ -3,7 +3,7 @@
 # wyclifeoluoch@gmail.com     #
 # Task: working with for loop #
 # Created 6th Jan 2021        #
-# Last edited 14th May 2021   #
+# Last edited 1th May 2021   #
 ###############################
 
 # The basic structure of a `for loop` is of the kind:
