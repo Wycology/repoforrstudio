1e28f2ec10b00ea07846f59af38667fe1a6d2d4d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-05-25 09:54:17 +0300

    library loading

--- dplyr.R
+++ dplyr.R
@@ -10,7 +10,7 @@
 library(tidyverse) # For data wrangling in the whole project.
 library(broom)
 library(nycflights13) # This is having the titanic data-set.
-library(moderndive) # Holds 
+library(moderndive)  
 # I will use the Titanic data for today's exploration
 
 titanic_df <- data.frame(Titanic) # Titanic data as a data.frame object
