94817efbb0dedcf5b055abb37dfcfb3e8137634c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-03 11:06:21 +0300

    from table to data.frame and to tibble

--- dplyr.R
+++ dplyr.R
@@ -19,6 +19,7 @@ titanic_df <- data.frame(Titanic) # Titanic data as a data.frame object
 # Once it is a data.frame, I can set it as a tibble which is my preferred structure
 titanic_tibble <- tibble(titanic_df) # Here is the tibble form of the titanic_df
 
+
 dim(titanic_tibble) # Checking the dimension of the data set; rows and columns.
 
 # Checking the total number of passengers in the Titanic
