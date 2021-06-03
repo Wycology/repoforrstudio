3722e7d70af18f724e1b47a936dfa0e7c603e333
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-03 11:04:28 +0300

    Model output

--- dplyr.R
+++ dplyr.R
@@ -8,7 +8,7 @@
 #########################################
 
 library(tidyverse) # For data wrangling in the whole project.
-library(broom) # For tidying model outputs. Kind of putting output in a tibble
+library(broom) # For tidying model outputs. Kind of displaying model in a tibble
 library(nycflights13) # This is having the titanic data-set.
 library(moderndive) # Almost the same role as broom of tidying model output 
 
