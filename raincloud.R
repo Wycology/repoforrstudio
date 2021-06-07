85264840f172179bc449799f45db823be1176ba9
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-07 14:22:46 +0300

    Better reformating of the code using Ctrl + Shift + A

--- raincloud.R
+++ raincloud.R
@@ -2,17 +2,21 @@
 library(ggplot2) # For plotting 
 library(ggdist)  # For adding the stat_halfeye and stat_dots
 
-ggplot(iris, aes(Species, Sepal.Length)) + 
-  stat_halfeye(adjust = .5, 
-                       width = .6, 
-                       .width = 0, 
-                       justification = -.2, 
-                       point_color = NA, 
-                       aes(fill = Species)) + 
-  geom_boxplot(width = .12, 
-               outlier.shape = NA) + 
-  stat_dots(side = 'left', 
-                    justification = 1.1, 
-                    binwidth = 0.05, 
-                    aes(col = Species)) + 
+ggplot(iris, aes(Species, Sepal.Length)) +
+  stat_halfeye(
+    adjust = .5,
+    width = .6,
+    .width = 0,
+    justification = -.2,
+    point_color = NA,
+    aes(fill = Species)
+  ) +
+  geom_boxplot(width = .12,
+               outlier.shape = NA) +
+  stat_dots(
+    side = 'left',
+    justification = 1.1,
+    binwidth = 0.05,
+    aes(col = Species)
+  ) +
   coord_cartesian(xlim = c(1., NA))
