36d947519da7ced3adc846e9b7019b481e5dff9d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-14 08:46:44 +0300

    commit

--- raincloud.R
+++ raincloud.R
@@ -1,25 +1,27 @@
 # Revealing better information about data which may be hidden by boxplots
-library(ggplot2) # For plotting 
-library(ggdist)  # For adding the stat_halfeye and stat_dots functions
 
+library(tidyverse)  # For wrangling the data and plotting 
+library(ggdist)     # For adding the stat_halfeye and stat_dots functionalities
+library(ggtext)     # For making annotation on the plot
+library(gapminder)  # For getting the data to plot
+library(glue)       # For handling the text labels on the plot
 
-iris |> 
-  select(Species, Sepal.Length) |>       # Picking the two columns
-  ggplot(aes(Species, Sepal.Length)) +   # Throwing them to aesthetics
-  stat_halfeye(
-    adjust = .5,
-    width = .6,
-    .width = 0,
-    justification = -.2,
-    point_color = NA,
-    aes(fill = Species)
-  ) +
-  geom_boxplot(width = .12,             # Adding the boxplot too
-               outlier.shape = NA) +
-  stat_dots(
-    side = 'left',
-    justification = 1.1,
-    binwidth = 0.05,
-    aes(col = Species)
-  ) +
-  coord_cartesian(xlim = c(1., NA))    # Adjusting the x-axis limits
+annotation <- data.frame(x = 4.5, y = 35, label = c(glue(
+  "The data is from the <br> gapminder package<br> which you can easily install")))
+
+gapminder |>
+  select(continent, lifeExp) |>     
+  ggplot(aes(continent, lifeExp)) +   
+  stat_halfeye(adjust = .5, width = .6, .width = 0, justification = -.2,
+               point_color = NA, aes(fill = continent)) +
+  geom_boxplot(width = .12, outlier.shape = NA) +
+  stat_dots(side = 'left', justification = 1.1, binwidth = 0.5, aes(col = continent)) +
+  geom_richtext(data = annotation, aes(x = x, y = y, label = label), color = 'orange',
+                size = 7, angle = 0, fontface = 'bold') +
+  labs(title = 'Distribution of life expectancy across continents', subtitle = 'Between 1952 and 2007',
+    caption = paste0('Source: Data from gapminder, Version ', packageVersion('gapminder')
+    ), x = 'Continent', y = 'Life expectancy (years)') +
+  coord_cartesian(xlim = c(0.6, NA)) +  
+  theme_classic() +
+  theme(legend.position = 'none',
+        text = element_text(size = 20)) 
