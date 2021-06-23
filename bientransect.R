e8473ee6069bbd2d1d342c770ac8d3c2b850117e
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-23 20:14:41 +0300

    23

--- bientransect.R
+++ bientransect.R
@@ -3,7 +3,7 @@
 # wyclifeoluoch@gmail.com      #
 # Task working with BIEN  data #
 # Created 8th Jan 2021         #
-# Last edited th May 2021    #
+# Last edited  May 2021    #
 ################################
 
 # Working with transect data in bien library
