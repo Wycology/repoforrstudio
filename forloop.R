06246244c7244c3e851dfb5ed2156b2bf658a1b0
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-24 19:50:42 +0300

    date

--- forloop.R
+++ forloop.R
@@ -3,7 +3,7 @@
 # wyclifeoluoch@gmail.com     #
 # Task: working with for loop #
 # Created 6th Jan 2021        #
-# Last edited 14th June 2021  #
+# Last edited 24th June 2021  #
 ###############################
 
 # The basic structure of a `for loop` is of the kind:
