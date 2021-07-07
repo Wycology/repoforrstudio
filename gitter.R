18e4fe94683e874d7371aed8b3c86643e14a4437
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-07-07 18:42:26 +0300

    loading lib

--- gitter.R
+++ gitter.R
@@ -7,7 +7,7 @@
 # Edited: 7th July 2021       #
 ###############################
 
-# Loading tlibraries
+# Loading libraries
 
 library(vioplot)   # version 0.3.6 
 library(ggplot2)   # version 3.3.5
