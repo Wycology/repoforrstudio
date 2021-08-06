04372cd91958c6e03c8ea4a60f60c9966d86079d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-06 21:57:13 +0300

    base

--- diamonds.R
+++ diamonds.R
@@ -16,7 +16,7 @@ diamonds_df <- diamonds # Saving the diamonds data-set to a new object in R.
 
 utils::head(diamonds) # Checking the variables in the data-set for subsequent analyses
 base::nrow(diamonds) # Checking for the number of rows in the data. 53940 observations
-utils::ncol(diamonds) # Checking the number of variables in the data-set. 10 variables
+base::ncol(diamonds) # Checking the number of variables in the data-set. 10 variables
 
 diamonds_df %>% # Picking the diamonds data, and then
   slice_sample(n = 0.2*(nrow(diamonds))) %>% # Picking a portion of the data-set
