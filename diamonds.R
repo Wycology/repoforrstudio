fd24cc6d55327ac489f9841d0a422d6856356916
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-07 22:30:32 +0300

    ggplot2::

--- diamonds.R
+++ diamonds.R
@@ -21,7 +21,7 @@ base::ncol(diamonds) # Checking the number of variables in the data-set. 10 vari
 diamonds_df %>% # Picking the diamonds data, and then
   dplyr::slice_sample(n = 0.2*(nrow(diamonds))) %>% # Picking a portion of the data-set
   ggplot2::ggplot(ggplot2::aes(x = cut, y = price, color = cut)) + # Adding the mapping variables
-  geom_point(position = "jitter", alpha = 0.2) + # Adding jitter and alpha
+  ggplot2::geom_point(position = "jitter", alpha = 0.2) + # Adding jitter and alpha
   geom_boxplot(alpha = 0.1, width = 0.5) + # Modifying dimensions of the boxplot 
   geom_violin(alpha = 0.5) + # Adding violin plot on top of the box and jitter
   theme_classic() + # Adding my preferred theme for publication.
