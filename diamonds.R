eaf16df7ea3682bed616a033b3606f345292ea73
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-07 22:32:13 +0300

    ggplot2::

--- diamonds.R
+++ diamonds.R
@@ -33,7 +33,7 @@ diamonds_df %>% # Picking the diamonds data, and then
        caption = "Data source: gglot2 package's diamond data") +
   ggplot2::theme(axis.text = element_text(size=16), # Size of labels on tick-marks
         axis.title = element_text(size=16,face="bold")) + # Size of axis labels
-  theme(plot.title = element_text(color = 'red', size = 18, face = 'bold'),
+  ggplot2::theme(plot.title = element_text(color = 'red', size = 18, face = 'bold'),
         plot.subtitle = element_text(color = 'purple', size = 14, face = 'italic'),
         plot.caption = element_text(color = 'blue', face = 'italic'))
 
