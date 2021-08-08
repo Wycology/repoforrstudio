9510aea30962e50a5880e4d181f650a785e32b87
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-08 20:27:47 +0300

    omm

--- diamonds.R
+++ diamonds.R
@@ -31,7 +31,7 @@ diamonds_df %>% # Picking the diamonds data, and then
        title = "Diamond cut against price", # Setting tibble of the plot
        subtitle = "Preview of diamond cut versus price", # Descriptive title beneath TITLE
        caption = "Data source: gglot2 package's diamond data") +
-  ggplot2::theme(axis.text = element_text(size=16), # Size of labels on tick-marks
+  ggplot2::theme(axis.text = ggplot2::element_text(size=16), # Size of labels on tick-marks
         axis.title = element_text(size=16,face="bold")) + # Size of axis labels
   ggplot2::theme(plot.title = element_text(color = 'red', size = 18, face = 'bold'),
         plot.subtitle = element_text(color = 'purple', size = 14, face = 'italic'),
