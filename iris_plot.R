653e73ce78be68d259caddcead5227afd7ce4f70
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-18 22:01:17 +0300

    this is a base thing

--- iris_plot.R
+++ iris_plot.R
@@ -20,8 +20,8 @@ utils::str(iris) # Checking the data structure. We can already see relatively hi
 # values for sepal length variable compared to the others.
 
 reg <- stats::lm(Petal.Width ~ Sepal.Length, data = iris) # Creating a regression line
-coeff <- lm::coefficients(reg)
-eq <- paste0('y = ', round(coeff[2], 1), "*x ", round(coeff[1], 1))
+coeff <- stats::coefficients(reg)
+eq <- base::paste0('y = ', round(coeff[2], 1), "*x ", round(coeff[1], 1))
 plot(iris$Sepal.Length, iris$Petal.Width, # Variables to use on the plot
      col = 'purple', # Color for the data points on the plot
      main = paste0("Simple plot in R \n Isn't it?\n", eq), # Title of the plot in two lines
