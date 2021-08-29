23a48ff782de80d136b627e2ff069ce37329003b
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-29 11:08:24 +0300

    progress = TRUE

--- readcsvs.R
+++ readcsvs.R
@@ -17,7 +17,7 @@ my_csv_files <- base::list.files(path = 'D:/FILES',
   dplyr::bind_cols() %>% # Binds the read files by columns
   dplyr::select(A...1, B...8, E...17) # Selects column names of interest
 
-readr::write_csv(my_csv_files, 'my_columns.csv', ) # Writes the output as csv to directory
+readr::write_csv(my_csv_files, 'my_columns.csv', progress = TRUE) # Writes the output as csv to directory
 
 # Note: Column names will be appended ...1, ...2, ...3 upto the last column name
 # This helps avoid having same column names in a single dataframe.
