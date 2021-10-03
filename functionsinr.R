aa013d0fe52af413c3c568fda70ac2d0671a410c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-03 20:47:03 +0300

    c

--- functionsinr.R
+++ functionsinr.R
@@ -4,7 +4,7 @@
 # GitHub: https://github.com/Wycology   #
 # Task: Creating functions in R         #
 # Date: 7th Jan 2020                    #
-# Last edited: 3r Sept 2021           #
+# Last edited: 3rd Sept 2021           #
 #########################################
 
 # R has thousands of in-built functions as well as those inside various third 
