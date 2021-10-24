4518cb87fa82b723cae252c251731bad08e9c9e6
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-10-24 21:12:09 +0300

    c

--- readcsvs.R
+++ readcsvs.R
@@ -12,7 +12,7 @@ base::library(readr)
 
 my_csv_files <- base::list.files(path = 'D:/FILES', 
                            pattern = "*.csv", 
-                           full.names = TRUE) %>% # Tells where the files are
+                           full.names = TRUE) %>% 
   base::lapply(readr::read_csv) %>% # Reads all the .csv files
   dplyr::bind_cols() %>% # Binds the read files by columns
   dplyr::select(A...1, B...8, E...17) # Selects column names of interest
