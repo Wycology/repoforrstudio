e8abfd40f5174faa880226165701fdc28fe1ac8d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-11-04 19:45:11 +0300

    c

--- forloop.R
+++ forloop.R
@@ -6,7 +6,7 @@
 # Last edited 4th Nov 2021    #
 ###############################
 
-# The basic structure of a `for loop` is of t :
+# The basic structure of a `for loop` is:
 
 for (item in list_of_items){
   do_something(item)
